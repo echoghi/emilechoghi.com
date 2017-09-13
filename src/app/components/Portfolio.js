82dcb6c8e01fc5842ef5b846f18f0d49d779bc22
emilec <emilec@corp.doctor.com>
2017-09-12 19:47:39 -0700

    Flesh out portfolio page

--- src/app/components/Portfolio.js
+++ src/app/components/Portfolio.js
@@ -1,15 +1,44 @@
 import React from 'react';
 // Components
 import NavBar from './NavBar';
+import Project from './Project';
 
 class Portfolio extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-      		loading : true,
-      		error   : null
+      		loading  : true,
+      		error    : null,
+      		projects : [
+      			{
+      				title: 'PiDash',
+      				date: 'July 2nd, 2017',
+      				text: 'A dashboard app for Raspberry Pi using React',
+      				tech: ['React', 'Webpack', 'SCSS'],
+      				key: 1
+      			},
+      			{
+      				title: 'Ninety Music',
+      				date: 'July 2nd, 2017',
+      				text: 'A music player powered by the SoundCloud API',
+      				tech: ['React', 'Webpack', 'SCSS'],
+      				key: 2
+      			}
+      		]
 		};
+	}
+
+	renderProjects() {
+		let portfolio = [];
+
+		this.state.projects.map(function(p) {
+			portfolio.push(
+				<Project title={p.title} date={p.date} description={p.text} key={p.key} chips={p.tech}/>
+			);
+		});
+
+		return portfolio;
 	}
 
 	render() {
@@ -23,6 +52,8 @@ class Portfolio extends React.Component {
 					<h4 className="portfolio__header">
 						Some Recent Projects
 					</h4>
+
+					{this.renderProjects()}
 				</div>
 			</div>
 		);
---
+++ src/app/components/Project.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Chip from 'material-ui/Chip';
+
+class Project extends React.Component {
+	renderChips() {
+		let chips = [];
+		let styles = {
+			margin: 4
+		};
+
+		this.props.chips.map(function(c, i) {
+			console.log(c);
+			chips.push(
+				<Chip style={styles} key={i}>
+		          {c}
+		        </Chip>
+			);
+		});
+
+		return chips;
+	}
+
+	render() {
+		return (
+			<div className="portfolio__item">
+				<div className="portfolio__item--preview">
+				</div>
+				<div className="portfolio__item--info">
+					<div>{this.props.title}</div>
+					<div>{this.props.date}</div>
+					<div>{this.props.description}</div>
+					<div className="chips">
+						{this.renderChips()}
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+};
+
+export default Project;
