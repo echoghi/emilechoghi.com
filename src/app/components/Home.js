8e8b387dba3854eedb453a7d0666cf1754512c78
emilec <emilec@corp.doctor.com>
2017-10-11 22:57:34 -0700

    Preliminary homepage design/styling

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { activatePage } from './actions';
 // Components
 import NavBar from './NavBar';
+// Images
+import homeImg from '../assets/images/home.png';
 
 const mapStateToProps = state => ({
     home: state.navigationState.home
@@ -32,7 +34,15 @@ class Home extends React.Component {
 			<div>
 				<NavBar />
 				<div className="home">
-					home
+					<div className="jumbotron">
+						<div className="jumbotron__container">
+							<div className="jumbotron__content">
+								<h1>Emile Choghi</h1>
+								<h2>Frontend Engineer</h2>
+							</div>
+						</div>
+					</div>
+					<img src={homeImg} />
 				</div>
 			</div>
 		);
