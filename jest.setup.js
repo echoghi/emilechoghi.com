a73d47c4d85d20e1f995c233bff76cbc882f9ded
emilec <emilec@corp.doctor.com>
2018-09-27 22:01:03 -0700

    updated tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import { mount, shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+global.React = React;
+global.Adapter = Adapter;
+global.mount = mount;
+global.shallow = shallow;
+global.configure = configure;
 // Quick NODE_ENV hack for Jest
 window.NODE_ENV = 'development';
 window.scrollTo = () => {};
--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,39 +1,7 @@
-import React from 'react';
-import { mount, shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import Contact from '../app/components/Contact.js';
 import 'whatwg-fetch';
 
 configure({ adapter: new Adapter() });
-
-test('Contact Form Should Submit', () => {
-    const component = mount(<Contact />);
-    const shallowComponent = shallow(<Contact />);
-    expect(shallowComponent).toMatchSnapshot();
-
-    const nameInput = component
-        .find('input')
-        .first()
-        .hostNodes()
-        .instance();
-    nameInput.value = 'Test';
-
-    const emailInput = component
-        .find('input')
-        .at(1)
-        .hostNodes()
-        .instance();
-    emailInput.value = 'test@jest.com';
-
-    const messageInput = component
-        .find('textarea')
-        .first()
-        .hostNodes()
-        .instance();
-    messageInput.value = 'testing form';
-
-    component.find('form').simulate('submit');
-});
 
 const mockFormPost = () => {
     return fetch('http://localhost:3000/api/postForm', {
@@ -53,8 +21,47 @@ const mockFormPost = () => {
         });
 };
 
-describe('Contact API', function() {
-    it('POST', async function() {
+describe('<Contact />', () => {
+    test('Contact Snapshot', () => {
+        const component = shallow(<Contact />);
+        expect(component).toMatchSnapshot();
+    });
+
+    test('Contact form renders correctly', () => {
+        const component = shallow(<Contact />);
+        // Test that component exists
+        expect(component).toBeDefined();
+        expect(component).toHaveLength(1);
+    });
+
+    test('Contact Form Should Submit', () => {
+        const component = mount(<Contact />);
+
+        const nameInput = component
+            .find('input')
+            .first()
+            .hostNodes()
+            .instance();
+        nameInput.value = 'Test';
+
+        const emailInput = component
+            .find('input')
+            .at(1)
+            .hostNodes()
+            .instance();
+        emailInput.value = 'test@jest.com';
+
+        const messageInput = component
+            .find('textarea')
+            .first()
+            .hostNodes()
+            .instance();
+        messageInput.value = 'testing form';
+
+        component.find('form').simulate('submit');
+    });
+
+    test('form should POST successfully', async function() {
         const response = await mockFormPost();
 
         expect(response.message).toBe('Email Sent');
