192eeedbdf79ddf997427a7fd347a52d6d1c24e2
emilec <emilec@corp.doctor.com>
2019-01-03 23:23:39 -0800

    navbar fix

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import Brand from './Brand';
-import Menu from './Menu';
-import { Nav } from './styles';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { NavBrand, Nav } from './styles';
+import Hamburger from './Hamburger';
+import { Links, Link } from './styles';
 
-const NavBar = () => (
-    <Nav>
-        <Brand />
-        <Menu />
-    </Nav>
-);
+const handleActivePath = path => {
+    if (window.location.pathname === path) {
+        return 'active';
+    } else {
+        return null;
+    }
+};
+
+const NavBar = () => {
+    const [open, handleMenu] = useState(false);
+
+    return (
+        <Nav>
+            <NavBrand onClick={() => handleMenu(false)}>
+                <NavLink to="/">
+                    <i className="icon-brand" />
+                </NavLink>
+            </NavBrand>
+            <Hamburger open={open} onClick={() => handleMenu(!open)} />
+            <Links open={open} role="menu">
+                <Link
+                    onClick={() => handleMenu(false)}
+                    className={handleActivePath('/')}
+                    to="/"
+                    exact
+                >
+                    Home <i className="icon-home" />
+                </Link>
+                <Link
+                    onClick={() => handleMenu(false)}
+                    className={handleActivePath('/portfolio')}
+                    to="/portfolio"
+                >
+                    Portfolio <i className="icon-briefcase" />
+                </Link>
+                <Link
+                    onClick={() => handleMenu(false)}
+                    className={handleActivePath('/contact')}
+                    to="/contact"
+                >
+                    Contact <i className="icon-message-square" />
+                </Link>
+            </Links>
+        </Nav>
+    );
+};
 
 export default NavBar;
