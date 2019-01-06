3709ca8c84a54d3cd8f09e306e9902fe33f63bbf
emilec <emilec@corp.doctor.com>
2019-01-06 12:00:36 -0800

    refactor

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -15,26 +15,30 @@ function NavBar() {
         }
     };
 
-    const linkProps = {
-        onClick: () => handleMenu(false)
-    };
-
     return (
         <Nav>
-            <Brand {...linkProps} />
+            <Brand onClick={() => handleMenu(false)} />
 
             <Hamburger open={open} onClick={() => handleMenu(!open)} />
 
             <Links open={open} role="menu">
-                <Link active={handleActivePath('/')} to="/" exact {...linkProps}>
+                <Link active={handleActivePath('/')} to="/" exact onClick={() => handleMenu(false)}>
                     Home <i className="icon-home" />
                 </Link>
 
-                <Link active={handleActivePath('/portfolio')} to="/portfolio" {...linkProps}>
+                <Link
+                    active={handleActivePath('/portfolio')}
+                    to="/portfolio"
+                    onClick={() => handleMenu(false)}
+                >
                     Portfolio <i className="icon-briefcase" />
                 </Link>
 
-                <Link active={handleActivePath('/contact')} to="/contact" {...linkProps}>
+                <Link
+                    active={handleActivePath('/contact')}
+                    to="/contact"
+                    onClick={() => handleMenu(false)}
+                >
                     Contact <i className="icon-message-square" />
                 </Link>
             </Links>
